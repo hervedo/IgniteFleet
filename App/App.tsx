745fde9882b7808b16e92680ada722d3fd7d1a1e
Hernandez Vedovatto <hernandez.vedovatto@gmail.com>
2024-05-20 15:36:26 -0300

    Login Social implementado

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from "styled-components/native";
-import { SignIn } from "./src/screens/signIn";
+import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "./src/components/Loading";
 import { StatusBar } from "react-native";
+import { AppProvider, UserProvider } from "@realm/react";
+import { REALM_APP_ID } from "@env";
+import { Routes } from "./src/routes";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { ANDROID_CLIENT_ID } from "@env";
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,9 +24,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
-      <SignIn />
-    </ThemeProvider>
+
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AppProvider>
+
   )
 }
---
+++ App/src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+
+
+import { HomeHeader } from '../../components/HomeHeader'
+import { Container } from './styles'
+
+export function Home() {
+    return (
+        <Container>
+            <HomeHeader />
+
+        </Container>
+    )
+}
